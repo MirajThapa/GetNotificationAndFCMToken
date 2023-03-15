8a3591e30f10cf7dbf509bae5704652b9e1b162e
unknown <thapamiraj34@gmail.com>
2023-03-15 11:23:17 +0545

    FCM token generated

---
+++ app/src/main/java/com/example/firebaseproject/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.firebaseproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.firebaseproject.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding activityMainBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("aa", "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+                        System.out.println(token);
+                        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+                        Log.i("cccc", token);
+                        Log.i("cccc", "Hello");
+                        activityMainBinding.name.setText(token);
+                    }
+                });
+    }
+}
